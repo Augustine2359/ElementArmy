67f8a476b9b2c8bd66a859100565cbf50ac0932f
NebulaMac1 <jiawenchong@nebulamac1.(none)>
2012-12-11 20:54:35 +0800

    tesettttt

---
+++ ElementArmy1.0/NBClasses/AdvanceClasses/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  ElementArmy1.0
+//
+//  Created by NebulaMac1 on 11/12/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ ElementArmy1.0/NBClasses/AdvanceClasses/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  ElementArmy1.0
+//
+//  Created by NebulaMac1 on 11/12/12.
+//
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
