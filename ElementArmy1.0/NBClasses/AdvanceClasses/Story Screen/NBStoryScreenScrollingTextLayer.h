5e0a4f0b5060496c654a1bad9e3ba5362a7dfe1e
Augustine Cheong <augustine.cheong@2359media.com>
2013-01-17 23:06:54 +0800

    Simple scrolling pictures and text

---
+++ ElementArmy1.0/NBClasses/AdvanceClasses/Story Screen/NBStoryScreenScrollingTextLayer.h
@@ -0,0 +1,13 @@
+//
+//  NBStoryScreenScrollingTextLayer.h
+//  ElementArmy1.0
+//
+//  Created by Augustine on 17/1/13.
+//
+//
+
+#import "CCLayer.h"
+
+@interface NBStoryScreenScrollingTextLayer : CCLayer
+
+@end
